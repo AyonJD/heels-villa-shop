4627c32c04b1a13f6fabe22ce4aa6b285eb2939a
Ayon <ayonjodder177@gmail.com>
2022-03-29 17:09:46 +0600

    All done

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
-import { addToLocalStorage } from "../Utilities/fakedb";
+import { addToLocalStorage, getFromLocalStorege } from "../Utilities/fakedb";
 import "./Shop.css";
 
 const Shop = () => {
@@ -15,13 +15,35 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  // Local storage load---------->
+  useEffect(() => {
+    //Step -1) Get the local storage data
+    const localData = getFromLocalStorege();
+
+    //Step -2) Local storage theke jehetu id er name r quantity paschi tai id er name theke product ta khuje ber kore product gulo k 1 ta array te rakhte hobe, tai 1 ta empty array banate hobe.
+    const localStorageProduct = [];
+
+    //Step -3) local storage theke paoa id gulo diye oi product gula bar korte hobe. Jehetu id qnique tai find use korte hobe cause full products array er vitor same id 2 ta nai.
+    //Jehetu local storage er theke j data paschi seita object akare asche tai loop caliya data theke id ta bar kore niye tarpore find korte hobe.
+    for (const id in localData) {
+      const matchedProduct = products.find(product => product.id === id);
+      
+      //Step -4) Jodi local storage thake kono product paoa jay tobe matchedProduct paoa jabe alada alada object hisabe. Jodi product paoa jay tobe local storage theke paoa data er vitor j id ache oita diye quantity k access kore match kora product gular quantity set kore dite hobe.
+      if (matchedProduct) {
+        matchedProduct.quantity = localData[id]
+        localStorageProduct.push(matchedProduct)
+      }
+    }
+    setCart(localStorageProduct)
+  }, [products])
+
   const handleAddToCart = (selectedProduct) => {
     let newCart = [];
     let quantity = selectedProduct.quantity;
     // console.log(quantity);
-    const exists = cart.find(product => product.id == selectedProduct.id)
+    const exists = cart.find(product => product.id === selectedProduct.id)
     if (exists) {
-      const rest = cart.filter(product => product.id != selectedProduct.id);
+      const rest = cart.filter(product => product.id !== selectedProduct.id);
       selectedProduct.quantity = quantity + 1;
       newCart = [...rest, selectedProduct];
     } else {
@@ -30,12 +52,16 @@ const Shop = () => {
     }
     setCart(newCart);
 
-    // Local Storege------------>
-    addToLocalStorage(selectedProduct.id)
+    // <-----------------------------------Local Storege-------------------------------->
+    //Adding product id and quantity to local storage
+    addToLocalStorage(selectedProduct.id);
+    //Getting product id and quantity from local storage-----see top useEffect
   };
 
   const handleClearCart = () => {
-    setCart([])
+    setCart([]);
+    //Removing the local storage data while clicking the delete all button
+    localStorage.removeItem('cart');
   };
 
   return (
--- src/Utilities/fakedb.js
+++ src/Utilities/fakedb.js
@@ -4,15 +4,15 @@ const getFromLocalStorege = () => {
 
 const addToLocalStorage = id => {
     const cart = getFromLocalStorege()
-    //local storage dta k wrap korar jonno 1 ta object declare korte hobe, er vitor sob selected product add korte hobe
+    //Step -1) local storage data k wrap korar jonno 1 ta object declare korte hobe, er vitor sob selected product add korte hobe
     let shoppingCart = {};
-    //Local storage er vitor kichu na thakle means !cart hole wrap kora object er vitor incoming id k key r er value set kore dibo one.
+    //Step -2) Local storage er vitor kichu na thakle means !cart hole wrap kora object er vitor incoming id k key r er value set kore dibo one.
     if (!cart) {
         shoppingCart[id] = 1;
     } else {
-        //Local storage er vitor kichu thakle seta k wrap kore dibo wrap korar jonno j empty object chilo oita diye. mane wrap kora object  = cart
+        //Step -3) Local storage er vitor kichu thakle seta k wrap kore dibo wrap korar jonno j empty object chilo oita diye. mane wrap kora object  = cart
         shoppingCart = cart;
-        //new id ta cart er vitor already thakle seita k dhore tar value 1 baray dibo r na thakle value set kore dibo 1.
+        //Step -4) New id ta cart er vitor already thakle seita k dhore tar value 1 baray dibo r na thakle value set kore dibo 1.
         if (shoppingCart[id]) {
             shoppingCart[id] = shoppingCart[id] + 1;
         } else {
